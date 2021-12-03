e760fda679cf6bbd6e71dc820148594941d18a81
Marcos <nodmarcos@gmail.com>
2021-12-03 17:49:51 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,61 @@
+var menus = require('../inc/menus');
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  menus.getMenus()
+  .then(results => {
+    res.render('index', {
+      title: 'Restaurante Saboroso!',
+      menus: results
+    })
+  })
+  
+});
+
+router.get('/contacts', function(req, res, next) {
+  res.render('contacts', {
+    title: 'Contato - Restaurante Saboroso!',
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga um oi!'
+
+  })
+})
+
+
+router.get('/menus', function(req, res, next) {
+  
+  menus.getMenus()
+  .then(results => {
+    res.render('menus', {
+      title: 'Menus - Restaurante Saboroso!',
+      background: 'images/img_bg_1.jpg',
+      h1: 'Saboreie nosso menu!',
+      menus: results
+    })
+  })
+})
+
+
+router.get('/reservations', function(req, res, next) {
+  res.render('reservations', {
+
+    title: 'Reservas - Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1: 'Reserve um mesa!'
+
+  })
+})
+
+
+router.get('/services', function(req, res, next) {
+  res.render('services', {
+    title: 'Serviços - Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer poder servir!'
+
+  })
+})
+
+module.exports = router;
